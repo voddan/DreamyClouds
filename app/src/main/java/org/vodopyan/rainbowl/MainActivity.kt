8fc63782383336a0d6e9a2dbb974140b8a214f17
voddan <dgvodopyan@gmail.com>
2019-10-20 23:15:12 +0300

    Added mediaPlayer with 2 buttons Bug - looping sound has a gap

--- app/src/main/java/org/vodopyan/rainbowl/MainActivity.kt
+++ app/src/main/java/org/vodopyan/rainbowl/MainActivity.kt
@@ -1,12 +1,26 @@
 package org.vodopyan.rainbowl
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.rain)
+        mediaPlayer.isLooping = true
+    }
+
+    fun startPlayer(view: View) {
+        mediaPlayer.start()
+    }
+
+    fun stopPlayer(view: View) {
+        mediaPlayer.pause()
     }
 }
