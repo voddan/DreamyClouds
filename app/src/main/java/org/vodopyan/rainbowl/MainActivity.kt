af4ef6245f1811c5541d27dc63a1b15371a1f826
voddan <dgvodopyan@gmail.com>
2019-10-24 00:54:08 +0300

    Fixed the gap in looping sounds

--- app/src/main/java/org/vodopyan/rainbowl/MainActivity.kt
+++ app/src/main/java/org/vodopyan/rainbowl/MainActivity.kt
@@ -1,26 +1,46 @@
 package org.vodopyan.rainbowl
 
-import android.media.MediaPlayer
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.source.LoopingMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DataSpec
+import com.google.android.exoplayer2.upstream.RawResourceDataSource
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mediaPlayer: MediaPlayer
+    lateinit var mediaPlayer: ExoPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.rain)
-        mediaPlayer.isLooping = true
+        mediaPlayer = buildLoopingPlayer(this, R.raw.rain)
     }
 
     fun startPlayer(view: View) {
-        mediaPlayer.start()
+        mediaPlayer.playWhenReady = true
     }
 
     fun stopPlayer(view: View) {
-        mediaPlayer.pause()
+        mediaPlayer.playWhenReady = false
     }
+}
+
+fun buildLoopingPlayer(context: Context, rawResourceId: Int): ExoPlayer {
+    val uri = RawResourceDataSource.buildRawResourceUri(rawResourceId)
+    val dataSource = RawResourceDataSource(context)
+    dataSource.open(DataSpec(uri))
+
+    val audioSource = ProgressiveMediaSource.Factory {dataSource}.createMediaSource(uri);
+    val loopingAudio = LoopingMediaSource(audioSource)
+
+    val player = ExoPlayerFactory.newSimpleInstance(context)
+    player.prepare(loopingAudio)
+
+    return player
 }
