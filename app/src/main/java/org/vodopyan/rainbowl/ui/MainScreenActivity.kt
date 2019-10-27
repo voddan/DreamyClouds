37e5fe79c329168877333657503fcff33ed9fabc
voddan <dgvodopyan@gmail.com>
2019-10-27 14:59:04 +0300

    Refactored MainScreenActivity.kt to use a list of players; Reformatted layout xmls

--- app/src/main/java/org/vodopyan/rainbowl/ui/MainScreenActivity.kt
+++ app/src/main/java/org/vodopyan/rainbowl/ui/MainScreenActivity.kt
@@ -1,9 +1,16 @@
 package org.vodopyan.rainbowl.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.player_controls.view.*
+import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
 import org.vodopyan.rainbowl.R
 import org.vodopyan.rainbowl.model.AppDataModel
@@ -22,6 +29,8 @@ class MainScreenActivity : AppCompatActivity() {
         volumeSeekBar.setOnSeekBarChangeListener(buildVolumeBarChangeListener(playerState, volumeSeekBar))
         playButton.setOnClickListener { playerState.isPlaying = true }
         stopButton.setOnClickListener { playerState.isPlaying = false }
+
+        playersList.adapter = PlayersListAdapter(get(), dataModel.players)
     }
 }
 
@@ -41,5 +50,24 @@ fun buildVolumeBarChangeListener(mediaPlayer: PlayerState, volumeBar: SeekBar): 
 
         override fun onStartTrackingTouch(seekBar: SeekBar) {}
         override fun onStopTrackingTouch(seekBar: SeekBar) {}
+    }
+}
+
+
+class PlayersListAdapter(context: Context, players: List<PlayerState>)
+    : ArrayAdapter<PlayerState>(context, R.layout.player_controls, players)
+{
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val playerState = getItem(position) ?: throw IndexOutOfBoundsException("No item at index $position")
+
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.player_controls, parent, /*attachToRoot=*/false)  // TODO: try reusing view
+
+        view.name.text = playerState.sound.name
+        view.volumeSeekBar.setOnSeekBarChangeListener(buildVolumeBarChangeListener(playerState, view.volumeSeekBar))
+        view.playButton.setOnClickListener { playerState.isPlaying = true }
+        view.stopButton.setOnClickListener { playerState.isPlaying = false }
+
+        return view
     }
 }
